3f2e9d3ee1e486deb48962bfca9cdc51d05fb204
Hugo Lesta <hugo.lesta@bestseller.com>
2021-11-16 12:33:20 +0100

    Fixes environment typo

--- {{cookiecutter.module_name}}/main.tf
+++ {{cookiecutter.module_name}}/main.tf
@@ -2,7 +2,7 @@ module "label1" {
   source      = "{{cookiecutter.source}}"
   namespace   = "{{cookiecutter.namespace}}"
   tenant      = "{{cookiecutter.tenant}}"
-  evinronment = "{{cookiecutter.evinronment}}"
+  environment = "{{cookiecutter.environment}}"
   stage       = "{{cookiecutter.stage}}"
   name        = "{{cookiecutter.name}}"
   label_order = "{{cookiecutter.label_order}}"
