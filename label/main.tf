9fdc08a034a83b12fd1edebd05741ec093c90d42
Hugo Lesta <hugo.lesta@bestseller.com>
2021-11-16 11:31:25 +0100

    Adds label as template

---
+++ label/main.tf
@@ -0,0 +1,15 @@
+module "label1" {
+  source      = {{cookiecutter.source}}
+  namespace   = {{cookiecutter.namespace}}
+  tenant      = {{cookiecutter.tenant}}
+  evinronment = {{cookiecutter.evinronment}}
+  stage       = {{cookiecutter.stage}}
+  name        = {{cookiecutter.name}}
+  attributes  = {{cookiecutter.attributes}}
+
+  label_order = {{cookiecutter.label_order}}
+
+  tags = {
+    "City"        = {{cookiecutter.city}}
+  }
+}
